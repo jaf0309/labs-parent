ba99e6703b27859444d70dd3cfe4723dad9b1c6f
zhujianfeng0309 <358008157@qq.com>
2022-10-21 21:36:49 +0800

    init

--- labx-30-spring-cloud-grpc/labx-30-grpc-cloud/labx-30-grpc-cloud-application/src/main/java/cn/iocoder/springcloud/labx30/demo/DemoApplication.java
+++ labx-30-spring-cloud-grpc/labx-30-grpc-cloud/labx-30-grpc-cloud-application/src/main/java/cn/iocoder/springcloud/labx30/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package cn.iocoder.springcloud.labx30.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
